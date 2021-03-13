f19c691b041473c7191f7bc92bbb182a15849c2c
Filipp Minayev <Filippmi@gmail.com>
2021-03-12 23:18:15 -0700

    Globe variables and adding global script

---
+++ javascripts/globals.js
@@ -0,0 +1,11 @@
+let projects = [];
+let leads = []
+const baseUrl = "http://localhost:3000"
+
+const main = () => document.getElementById("main");
+const nameInput = () => document.getElementById('name');
+const descriptionInput = () => document.getElementById('description');
+const pForm = () => document.getElementById("project_form");
+const pFormLink = () => document.getElementById("pform-link");
+const projectsLink = () => document.getElementById("my-projects");
+const leadNameInput = () => document.getElementById("lead-name");
