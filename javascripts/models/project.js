9bc58c1ce5ddc57eb03841ebe3475ea13fe5a2ae
Filipp Minayev <Filippmi@gmail.com>
2021-03-13 01:51:57 -0700

    refactored project class

--- javascripts/models/project.js
+++ javascripts/models/project.js
@@ -25,8 +25,8 @@ class Project{
     deleteLink.setAttribute("href", "#")
     deleteLink.innerText = "Delete"
   
-    editLink.addEventListener("click", editProject);
-    deleteLink.addEventListener("click", deleteProject);
+    editLink.addEventListener("click", Project.editProject);
+    deleteLink.addEventListener("click", Project.deleteProject);
   
     h4.innerText = `Project Name: ${this.name}`;
     leadName.innerText = `Team Lead: ${this.lead.name}`;
@@ -59,11 +59,152 @@ class Project{
     `
   }
 
+  static projectFormTemplate() {
+    return `
+    <h3>Create Project</h3>
+    <form id="project_form">
+      <div class="input-feild">
+        <label for="name">Project Name</label>
+        <input type="text" name="name" id="name"><br>
+      </div>
+      <div class="input-feild">
+        <label for="lead-name">Team Lead </label>
+        <input type="text" name="lead-name" id="lead-name"><br>
+      </div>
+      <div class="input-feild">
+        <label for="description">Project Description</label><br>
+        <textarea name="description" id="description" cols="30" rows="5"></textarea>
+      </div>
+      <input type="submit" value="Create Project">
+    </form>
+    `;
+  }
+
+  static editFormTemplate(project) {
+    return `
+    <h3>Edit Project</h3>
+    <form id="project_form" data-id="${project.id}">
+    <div class="input-feild">
+    <label for="name">Project Name</label>
+    <input type="text" name="name" id="name" value="${project.name}"><br>
+    </div>
+    <div class="input-feild">
+    <label for="description">Description</label><br>
+    <textarea name="description" id="description" cols="30" rows="5">${project.description}</textarea>
+    </div>
+    <input type="submit" value="Edit">
+    </form>
+    `;
+  }
+
+  static renderPForm() {
+    resetMain();
+    main().innerHTML = Project.projectFormTemplate();
+    pForm().addEventListener("submit", Project.submitPForm);
+  }
+
+  static renderEditForm(project) {
+    resetMain();
+    main().innerHTML = Project.editFormTemplate(project);
+    pForm().addEventListener("submit", Project.submitEditProjectForm);
+  }
+
   static renderProjects() {
     // resetMain();
     main().innerHTML = Project.projectsTemp();
   
     Project.all.forEach(project => project.render());
     
+  }
+
+  static editProject(e) {
+    e.preventDefault();
+    const id = e.target.dataset.id;
+  
+    const project = Project.all.find(function(project) {
+      return project.id == id;
+    })
+  
+    Project.renderEditForm(project);
+  }
+
+  static submitPForm(e) {
+    e.preventDefault();
+  
+    let strongParams = {
+      project: {
+        name: nameInput().value,
+        description: descriptionInput().value,
+        lead_attributes: leadNameInput().value
+      }
+    }
+    fetch(baseUrl+"/projects", {
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(strongParams),
+      method: "POST"
+    })
+      .then( function(resp) {
+        return resp.json();
+      })
+      .then( function(data) {
+        Project.create(data)
+        Project.renderProjects();
+      })
+  }
+
+  static submitEditProjectForm(e) {
+    e.preventDefault();
+  
+    let strongParams = {
+      project: {
+        name: nameInput().value,
+        description: descriptionInput().value
+      }
+    }
+    const id = e.target.dataset.id;
+  
+    fetch(baseUrl + "/projects/" + id, {
+      method: "PATCH",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(strongParams)
+    })
+    .then(function(resp) {
+      return resp.json();
+    })
+    .then(function(data) {
+      let p = Project.all.find(function(p) {
+        return p.id == data.id;
+      })
+      let idx = Project.all.indexOf(p);
+      Project.all[idx] = new Project(data);
+      alert(`${Project.name} was edited`)
+      Project.renderProjects();
+    })
+  }
+
+  static deleteProject(e) {
+    e.preventDefault();
+    
+    let id = e.target.dataset.id;
+  
+    fetch(baseUrl + "/projects/" + id, {
+      method: "DELETE"
+    })
+    .then(function(resp) {
+      return resp.json();
+    })
+    .then(function(data) {
+  
+      Project.all = Project.all.filter(function(project) {
+        return project.id !== data.id;
+      })
+      Project.renderProjects();
+    });
   }
 }
